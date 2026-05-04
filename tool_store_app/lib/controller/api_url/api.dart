781515b6e7adfac477067f9bfa475c13433925dc
40-Muhammad Ismail <m.ismail.26934@gmail.com>
2026-05-04 15:17:58 +0800

    Update tool_data.dart

--- tool_store_app/lib/controller/api_url/api.dart
+++ tool_store_app/lib/controller/api_url/api.dart
@@ -1,6 +1,6 @@
 class ApiUrl {
-  static String server = 'http://192.168.1.29:8080/';
-  // static String server = 'http://10.74.115.69:8080/';
+  // static String server = 'http://192.168.1.29:8080/';
+  static String server = 'http://10.164.89.69:8080/';
   static String folderApiTool = 'api_tool';
   static String folderApiToolStore = 'api_toolstore';
   static String folderController = 'controller';
