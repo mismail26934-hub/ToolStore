7e2b7417e896b80689cf7a2f08ea9451d6fc8d2a
40-Muhammad Ismail <m.ismail.26934@gmail.com>
2026-05-23 21:52:13 +0800

    Feat Search Data Tool

--- tool_store_app/lib/controller/api_url/api.dart
+++ tool_store_app/lib/controller/api_url/api.dart
@@ -1,6 +1,6 @@
 class ApiUrl {
-  static String server = 'http://192.168.1.11:8080/';
-  // static String server = 'http://10.157.164.69:8080/';
+  static String serv = 'http://192.168.1.11:8080/';
+  // static String serv = 'http://10.157.164.69:8080/';
   static String fApiTool = 'api_tool';
   static String fApi = 'api_toolstore';
   static String fCnt = 'v1';
@@ -14,15 +14,14 @@ class ApiUrl {
   static String fSuperior = 'superior';
   static String fAlogin = 'auth/login';
 
-  static String contDataUser = "$server/$fApiTool/$fApi/$fCnt/$fUser";
-  static String contDataTool = "$server/$fApiTool/$fApi/$fCnt/$fForm";
-  static String contLogin = "$server/$fApiTool/$fApi/$fCnt/$fAlogin";
-  static String contDataToolDetail =
-      "$server/$fApiTool/$fApi/$fCnt/$fFormDetail";
+  static String contDataUser = "$serv/$fApiTool/$fApi/$fCnt/$fUser";
+  static String contDataTool = "$serv/$fApiTool/$fApi/$fCnt/$fForm";
+  static String contLogin = "$serv/$fApiTool/$fApi/$fCnt/$fAlogin";
+  static String contDataToolDetail = "$serv/$fApiTool/$fApi/$fCnt/$fFormDetail";
 
-  static String contPO = "$server/$fApiTool/$fApi/$fCnt/$fPo";
-  static String contSO = "$server/$fApiTool/$fApi/$fCnt/$fSo";
-  static String contSuperrior = "$server/$fApiTool/$fApi/$fCnt/$fSuperior";
-  static String contRcvWh = "$server/$fApiTool/$fApi/$fCnt/$fReceiveWh";
-  static String contRcvTool = "$server/$fApiTool/$fApi/$fCnt/$fReceiveTool";
+  static String contPO = "$serv/$fApiTool/$fApi/$fCnt/$fPo";
+  static String contSO = "$serv/$fApiTool/$fApi/$fCnt/$fSo";
+  static String contSuperrior = "$serv/$fApiTool/$fApi/$fCnt/$fSuperior";
+  static String contRcvWh = "$serv/$fApiTool/$fApi/$fCnt/$fReceiveWh";
+  static String contRcvTool = "$serv/$fApiTool/$fApi/$fCnt/$fReceiveTool";
 }
